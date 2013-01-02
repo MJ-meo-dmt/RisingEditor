5ae204f93e4619bd2e3b1bb5a080eeef4a11945c
fireclawthefox <fireclawthefox@gmail.com>
2013-01-02 17:20:22 +0100

    Extend ignore list by emacs ropeproject folder Change mouse view navigation key from alt to contrl to not interfere with window moving of OS like ubuntu Set Object viewer css to move the viewer to the left edge of the window and dock it there, so it moves if the window gets resized Add automatic scrollbar to file viewer clean imports of main.py set notify level of rocket to warning some small bugfixes and general code cleanup Add open and close functionality to file viewer Always add .. directory to the file viewer Set file viewer textbox value to current path on folder change Add avatar.egg model just to test loading other models Rename jump.egg file in SomeFolder to jump_In_SomeFolder.egg to determine it's the one in the sub directory and not the one in the base models dir.

--- src/gizmo_core/mouse.py
+++ src/gizmo_core/mouse.py
@@ -7,51 +7,51 @@ MOUSE_ALT = 0
 
 
 class Mouse( DirectObject ):
-    
+
     """Class representing the mouse."""
-    
+
     def __init__( self ):
         DirectObject.__init__( self )
-        
+
         self.x = 0
         self.y = 0
         self.dx = 0
         self.dy = 0
         self.buttons = [False, False, False]
         self.modifiers = []
-        
+
         # Bind button events
         self.accept( 'mouse1', self.SetButton, [0, True] )
-        self.accept( 'alt-mouse1', self.SetButton, [0, True, [MOUSE_ALT]] )
+        self.accept( 'control-mouse1', self.SetButton, [0, True, [MOUSE_ALT]] )
         self.accept( 'mouse1-up', self.SetButton, [0, False, []] )
-        
-        self.accept( 'alt-mouse2', self.SetButton, [1, True, [MOUSE_ALT]] )
+
+        self.accept( 'control-mouse2', self.SetButton, [1, True, [MOUSE_ALT]] )
         self.accept( 'mouse2', self.SetButton, [1, True] )
         self.accept( 'mouse2-up', self.SetButton, [1, False, []] )
-        
-        self.accept( 'alt-mouse3', self.SetButton, [2, True, [MOUSE_ALT]] )
+
+        self.accept( 'control-mouse3', self.SetButton, [2, True, [MOUSE_ALT]] )
         self.accept( 'mouse3', self.SetButton, [2, True] )
         self.accept( 'mouse3-up', self.SetButton, [2, False, []] )
-        
+
     def SetButton( self, id, value, modifiers=[] ):
-        
+
         # Record buttons and modifiers
         self.buttons[id] = value
         self.modifiers = modifiers
-        
+
     def __UpdateTask( self, task ):
-        
+
         # Get pointer from screen, calculate delta
         mp = getBase().win.getPointer( 0 )
         self.dx = self.x - mp.getX()
         self.dy = self.y - mp.getY()
         self.x = mp.getX()
         self.y = mp.getY()
-        
+
         return Task.cont
-    
+
     def Start( self ):
         taskMgr.add( self.__UpdateTask, 'mouseUpdateTask' )
-    
+
     def Stop( self ):
         taskMgr.remove( self.__UpdateTask )
--- src/main.py
+++ src/main.py
@@ -8,25 +8,24 @@ See License.txt or http://opensource.org/licenses/BSD-2-Clause for more info
 
 ## IMPORTS ##
 import os
-import sys
 
 from pandac.PandaModules import loadPrcFileData
 loadPrcFileData("",
-"""    
+"""
     window-title Rising Editor
     fullscreen 0
     win-size 1154 768
     cursor-hidden 0
     show-frame-rate-meter 1
-    notify-level-rocket debug
-    
+    notify-level-rocket warning
+
 """
 )
 
 ### PANDA Imports ###
 from direct.showbase.ShowBase import ShowBase
-from pandac.PandaModules import *
-from panda3d.rocket import *
+#from pandac.PandaModules import
+from panda3d.rocket import RocketRegion, RocketInputHandler, LoadFontFace
 from panda3d.core import Filename
 from direct.showbase.DirectObject import DirectObject
 
@@ -41,118 +40,115 @@ from level.LevelLoader import LevelLoader
 ### EDITOR BASE ###
 
 class Editor(ShowBase):
-    
+
     def __init__(self):
-        
+
         ShowBase.__init__(self)
-        
+
     #------------------------------------------------------------------#
     #
     #   SETUP BASIC LIBROCKET
     #
     #------------------------------------------------------------------#
-        
+
         ## LOAD FONT ##
         LoadFontFace("../data_src/gui/fonts/verdana.ttf")
-        
+
         ## SETUP REGION & INPUT HANDLER ##
         rw = RocketRegion.make('pandaRocket', base.win)
         rw.setActive(1)
-        
+
         ih = RocketInputHandler()
         base.mouseWatcher.attachNewNode(ih)
         rw.setInputHandler(ih)
-        
+
         ## CONTEXT ##
         self.context = rw.getContext()
-        
+
         ## SETUP MAIN EDITOR WINDOW & GUI ##
         self.editorGui = self.context.LoadDocument('gui_src/editor_main.rml')
         self.editorGui.Show()
-        
-        #self.dialog = self.context.LoadDocument('gui_src/editor_open_dialog.rml')
-        #self.dialog.Show()
-    
-    
+
+
     #------------------------------------------------------------------#
     #
     #   EDITOR DEFAULT SETUP
     #
     #------------------------------------------------------------------#
-        
+
         ## BASE DIR ##
         currentDir = os.getcwd()
-        self.baseDir = currentDir + "\models"
-        
-        
+        self.baseDir = currentDir + os.sep + "models"
+
+
     #------------------------------------------------------------------#
     #
     #   EDITOR EVENTS
     #
     #------------------------------------------------------------------#
-        
+
         self.accept('new', self.New)
         self.accept('open', self.Open)
         self.accept('save', self.Save)
         self.accept('import', self.Import)
         self.accept('export', self.Export)
-        
-        
+
+
         ########################
         # Just build a basic simple file folder loader viewer thing for now
         # Something that loads or shows the contents of a dir hard coded
-        # 
+        #
         # editor: model, model1, model2, model3
         # and the texture folders, so that when clicked they can be added into the editor scene
-        
-    
+
+
         # Temp gizmo runner
         self.gizmo = Gizmo(self)
-        
-        
-        ## 
+
+
+        ##
         # This should run when open file is used.
         self.levelload = LevelLoader(self)
         self.levelload.read("level/jump.lvlml", False)
         self.levelload.run()
-        
-        
+
+
     #------------------------------------------------------------------#
     #
     #   FILE DROP LIST
     #
     #------------------------------------------------------------------#
-    
+
     def New(self):
         """
         Handle the clearing for a new fresh scene.
         """
-        
+
         for node in self.gizmo.rootNp.getChildren():
-            print node
             node.remove_node()
-            
+
     def Open(self):
         """
         Handle the opening of a file browser dialog, browsing and
         opening of files selected.
         """
         self.fileBrowser = FileBrowser(self, self.baseDir)
-        
+        self.fileBrowser.open(self.context)
+
     def Save(self):
         """
         Handle the saving of the scene, maybe even write out to file.
         Also handle the lvlml writeout.
         """
-        pass 
-        
+        pass
+
     def Import(self):
         """
         Handle importing files.
         *Atm not needed
         """
-        pass 
-        
+        pass
+
     def Export(self):
         """
         Handle exporting files.
@@ -160,20 +156,20 @@ class Editor(ShowBase):
         *Not used atm
         """
         pass
-        
-        
+
+
     #------------------------------------------------------------------#
     #
     #   EDIT DROP LIST
     #
     #------------------------------------------------------------------#
-    
+
     #------------------------------------------------------------------#
     #
     #   VIEW DROP LIST
     #
     #------------------------------------------------------------------#
-    
+
     #------------------------------------------------------------------#
     #
     #   HELP DROP LIST
@@ -193,148 +189,155 @@ class Editor(ShowBase):
 ### EDITOR FILE BROWSER ###
 
 class FileBrowser(DirectObject):
-    
+
     def __init__(self, _base, baseDir):
-        
         self.base = _base
-        
         self.currentDir = baseDir
-        
-        print self.currentDir
         self.selectedFile = None
-        
+
+
+    def open(self, context):
+        self.dialog = context.LoadDocument('gui_src/editor_open_dialog.rml')
+        self.dialog.Show()
+
+        btnCancel = self.dialog.GetElementById("cancel_btn")
+        btnCancel.AddEventListener('click', lambda:self.close(), True)
+
+        self.viewerList = self.dialog.GetElementById("filepathlist")
+        self.viewerNavigationBox = self.dialog.GetElementById("path_txt")
+
         ## EVENTS ##
         self.accept('b', self.goBack)
-    
-        self.tempDirView = self.dirViewer(self.currentDir)
-    
+
+        self.openFolderDir(self.currentDir)
+
+
+    def close(self):
+        if self.dialog != None:
+            self.dialog.Close()
+
+
     def dirViewer(self, dir):
-        
+        # add the special directory ".." to the directory
+        # list to be able to navigate one directory level up
+        # split up the directory and use the absolut path
+        # instead of just adding .. at the end
+        tmpPathList = dir.split('/')
+        del tmpPathList[-1]
+        tmpPath = "/".join(tmpPathList)
+        if tmpPath == "": tmpPath = "/"
+        self.createElementDir("div", "..", tmpPath)
+
         ## LOOP DIR ##
         for root, dirs, files in os.walk(dir):
             for name in files:
                 filepath = os.path.join(root, name)
-    
+
                 if filepath.endswith(".egg"):
-                    
                     self.createElementModel("div", name, filepath)
-                    
+
                 if filepath.endswith(".lvlml"):
-                    
                     self.createElementLvlml("div", name, filepath)
-            
+
             for dir in dirs:
                 filepath = os.path.join(root, dir)
                 self.createElementDir("div", dir, os.path.join(root, dir))
-    
+            # just add the files and folders of the current directory
+            # and not of every directory in the tree
+            break
+
+
     def goBack(self):
-        
         self.tempDir = str(Filename.fromOsSpecific(self.currentDir))
-        
+
         filepathlist = self.tempDir.split('/')
         del filepathlist[-1]
-        
+
         self.tempStr = os.path.sep.join(filepathlist)
-        self.currentDir = self.tempStr
-        print filepathlist
         print self.tempStr
-    
-    
+        self.currentDir = self.tempStr
+        self.openFolderDir(self.tempStr)
+
+
     def createElementModel(self, element, data, filepath):
-        
-        # Get element on document
-        fileview = self.base.editorGui.GetElementById("filepathlist")
         # Create element
-        entry = self.base.editorGui.CreateElement(element)
+        entry = self.dialog.CreateElement(element)
         entry.SetAttribute("id", data)
-        entry.AddEventListener('click', lambda:self.importModelToScene(data, filepath), True)
+        entry.AddEventListener('click', lambda:self.importModelToScene(filepath), True)
         entry2 = self.base.editorGui.CreateElement('br')
-        
+
         # Add text to display
         entry.inner_rml = data
-        print data
         # Add element to base element
-        fileview.AppendChild(entry2)
-        fileview.AppendChild(entry)
-    
-    
+        self.viewerList.AppendChild(entry2)
+        self.viewerList.AppendChild(entry)
+
+
     def createElementLvlml(self, element, data, filepath):
-        
-        # Get element on document
-        fileview = self.base.editorGui.GetElementById("filepathlist")
         # Create element
-        entry = self.base.editorGui.CreateElement(element)
+        entry = self.dialog.CreateElement(element)
         entry.SetAttribute("id", data)
-        entry.AddEventListener('click', lambda:self.addModelToScene(data, filepath), True)
+        entry.AddEventListener('click', lambda:self.addModelToScene(filepath), True)
         entry2 = self.base.editorGui.CreateElement('br')
-        
+
         # Add text to display
         entry.inner_rml = data
-        print data
         # Add element to base element
-        fileview.AppendChild(entry2)
-        fileview.AppendChild(entry)
-        
+        self.viewerList.AppendChild(entry2)
+        self.viewerList.AppendChild(entry)
+
+
     def createElementDir(self, element, data, filepath):
-        
-        # Get element on document
-        fileview = self.base.editorGui.GetElementById("filepathlist")
         # Create element
-        entry = self.base.editorGui.CreateElement(element)
+        entry = self.dialog.CreateElement(element)
         entry.SetAttribute("id", data)
-        entry.AddEventListener('click', lambda:self.openFolderDir(data, filepath), True)
+        entry.AddEventListener('click', lambda:self.openFolderDir(filepath, True), True)
         entry2 = self.base.editorGui.CreateElement('br')
-        
+
         # Add text to display
         entry.inner_rml = data
-        print data
+
         # Add element to base element
-        fileview.AppendChild(entry2)
-        fileview.AppendChild(entry)
+        self.viewerList.AppendChild(entry2)
+        self.viewerList.AppendChild(entry)
 
 
-    def importModelToScene(self, path, filepath):
-        self.path = path 
-        
+    def importModelToScene(self, filepath):
         # Add a if type check in here so that it checks
         # if file.egg ask to open
         # el if not .egg say its not right is other type
-        # else if dir open that dir and display the files 
-        
+        # else if dir open that dir and display the files
+
         # use open for lvlml type files and normal egg files get imported
-        
+
         modelDir = Filename.fromOsSpecific(filepath)
-        
-        self.model = loader.loadModel(modelDir)
+
+        self.model = self.base.loader.loadModel(modelDir)
         self.model.reparentTo(self.base.gizmo.rootNp)
-        
-        
-        #print Filename.exists(modelDir)
-        #self.levelload = LevelLoader(self)
-        #self.levelload.read(str(modelDir), False)
-        #self.levelload.run()
-        
-    def addModelToScene(self, path, filepath):
-        self.path = path
-        
+
+
+    def addModelToScene(self, filepath):
         modelDir = Filename.fromOsSpecific(filepath)
-        
-        print Filename.exists(modelDir)
+
         self.levelload = LevelLoader(self)
         self.levelload.read(str(modelDir), False)
         self.levelload.run()
 
-    def openFolderDir(self, path, filepath):
-        self.path = path
-        print self.path, "Folder Dir"
-        #del self.tempDirView
-        fileview = self.base.editorGui.GetElementById("filepathlist")
-        for child in fileview.child_nodes:
-            print child, "this is one"
-            fileview.RemoveChild(child)
-            del child
-            
+
+    def openFolderDir(self, filepath, test=False):
+        self.currentDir = filepath
+        self.clearDirViewer()
         self.tempDirView = self.dirViewer(filepath)
+        self.viewerNavigationBox.value = self.currentDir
+
+
+    def clearDirViewer(self):
+        # remove all directories and files from the list
+        for elem in self.viewerList.GetElementsByTagName("div"):
+            self.viewerList.RemoveChild(elem)
+        # remove the hidden <br> elements
+        for elem in self.viewerList.GetElementsByTagName("br"):
+            self.viewerList.RemoveChild(elem)
 
 app = Editor()
 
