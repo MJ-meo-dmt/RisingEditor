b0bdf2d76eb46794ce5ee852bc09acf50a89caeb
MJ-meo-dmt <francois.debruyn66@gmail.com>
2013-01-29 23:53:33 +0200

    Added core sample files

---
+++ plugins/EditorCore.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+#----------------------------------------------------------------------#
+"""
+Editor Core.
+Mainly routing and forwarding functions, like the main control centre.
+
+"""
+
+# System imports
+import sys
+import os
+import logging
+
+# Panda Engine imports
+
+# Extra imports
+
+#----------------------------------------------------------------------#
+
+### EDITOR CORE ###
+
+class EditorCore():
+    
+    
+    def __init__(self):
+        
+        pass 
+        
+    
+    def start(self):
+        pass 
+        
+    def stop(self):
+        pass
